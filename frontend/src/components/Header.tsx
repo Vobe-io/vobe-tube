ea560d91bbd12b2222590ac94736b9f8dfe4e174
Timo Aeckerle <timoaeckerle@gmx.de>
2020-09-30 21:43:52 +0200

    Test

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -13,6 +13,7 @@ class Header extends Component {
         return (
             <div className={Style.Header}>
                 <Search />
+                <Profile />
             </div>
         );
     }
